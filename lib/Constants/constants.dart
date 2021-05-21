780cfa95585980a234133c72c0a095a0d90390dc
pierre edimo <pierredimo@live.com>
2021-05-21 14:38:19 +0200

    constants

---
+++ lib/Constants/constants.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+BorderRadius onlyTop = BorderRadius.only(
+  topLeft: Radius.circular(20.0),
+  topRight: Radius.circular(20.0),
+);
+
+TextStyle titleStyle = TextStyle(
+  fontWeight: FontWeight.bold,
+  fontSize: 18.0,
+  color: Color(0xffbffffff),
+);
