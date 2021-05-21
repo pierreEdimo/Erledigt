a98b8bf8c0960ace91b78906045db4b41a450561
pierre edimo <pierredimo@live.com>
2021-05-21 14:18:23 +0200

    custom container

---
+++ lib/Widgets/custom_container.dart
@@ -0,0 +1,20 @@
+import 'package:erledigt/Constants/constants.dart';
+import 'package:flutter/material.dart';
+
+class CustomContainer extends StatelessWidget {
+  final Widget? child;
+
+  CustomContainer({Key? key, this.child}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(25.0),
+      decoration: BoxDecoration(
+        color: Color(0xffbffffff),
+        borderRadius: onlyTop,
+      ),
+      child: child,
+    );
+  }
+}
