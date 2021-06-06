3caef4fb575104adcd3566a22a0d77b4b15d48a4
pierreEdimo <pedimonkoe@yahoo.com>
2021-06-06 16:30:21 +0200

    edit name_input

--- lib/Widgets/name_input.dart
+++ lib/Widgets/name_input.dart
@@ -3,12 +3,22 @@ import 'package:flutter/material.dart';
 Widget nameInput(
   TextEditingController controller,
   String hinText,
+  bool autofocus,
 ) {
   return TextFormField(
+    autofocus: autofocus,
     controller: controller,
+    minLines: 1,
+    maxLines: null,
+    style: TextStyle(
+      fontSize: 12.0,
+      fontFamily: 'OpenSans',
+    ),
     decoration: InputDecoration(
       border: InputBorder.none,
       hintText: hinText,
+      filled: true,
+      fillColor: Colors.grey.shade100,
     ),
   );
 }
