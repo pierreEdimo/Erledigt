e0be50aab3a8116ce5fe25a0e0c67d626165bdc2
pierreEdimo <pedimonkoe@yahoo.com>
2021-06-06 16:23:45 +0200

    edit about screen

--- lib/Screens/about_screen.dart
+++ lib/Screens/about_screen.dart
@@ -3,10 +3,11 @@ import 'package:erledigt/Widgets/custom_app_bar.dart';
 import 'package:erledigt/Widgets/custom_container.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 String _source =
-    "**Erledigt** is a simple ToDo application developped by Pierre Edimo. He inspired himself from" +
-        "a design he saw on dribbble. \n With **Erledigt** , you can create , read , update and delete List. When created you can " +
+    "**Erledigt** is a simple ToDo application developped by [Pierre Edimo](mailto:pierredimo@live.com). " +
+        "\n With **Erledigt** , you can create , read , update and delete List. When created you can " +
         "you can add a new Task , or multiple new Tasks. when the task is completed you can check it off. If you want you can edit a Task or delete it.";
 
 class AboutScreen extends StatelessWidget {
@@ -15,28 +16,39 @@ class AboutScreen extends StatelessWidget {
     return Scaffold(
       appBar: customAppBar(
         Container(
+          color: Color(0xffbffffff),
           padding: EdgeInsets.symmetric(horizontal: 25.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                'About',
-                style: titleStyle,
-              ),
-              IconButton(
-                icon: Icon(
-                  Icons.close_outlined,
-                  color: Color(0xFFBFFFFFF),
+          child: Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'About',
+                  style: titleStyle,
                 ),
-                onPressed: () => Navigator.of(context).pop(),
-              ),
-            ],
+                IconButton(
+                  icon: Icon(
+                    Icons.close_outlined,
+                  ),
+                  onPressed: () => Navigator.of(context).pop(),
+                ),
+              ],
+            ),
           ),
         ),
       ),
       body: CustomContainer(
         child: Markdown(
+          padding: EdgeInsets.zero,
           data: _source,
+          onTapLink: (text, String? url, title) {
+            launch(url!);
+          },
+          styleSheet: MarkdownStyleSheet(
+            p: TextStyle(
+              fontSize: 12.0,
+            ),
+          ),
         ),
       ),
     );
