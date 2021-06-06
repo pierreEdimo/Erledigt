2c0300342a46976af1a482fb6f0944a5ab43c95b
pierreEdimo <pedimonkoe@yahoo.com>
2021-06-06 16:25:58 +0200

    add crud for task

--- lib/Service/task_service.dart
+++ lib/Service/task_service.dart
@@ -1,102 +1,34 @@
-import 'dart:convert';
-
-import 'package:erledigt/Model/create_task.dart';
+import 'package:erledigt/Model/list_model.dart';
 import 'package:erledigt/Model/task.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:http/http.dart';
+import 'package:flutter/foundation.dart';
 
-import '../main.dart';
+import 'package:erledigt/boxes.dart';
 
 class TaskService extends ChangeNotifier {
-  Future<List<Task>> fetchTasks(int listId) async {
-    String? userId = await storage.read(key: 'userId');
+  final _taskBox = Boxes.getTasks();
 
-    Response response = await get(
-      Uri.parse(
-          'https://elerdigt-api.azurewebsites.net/api/ToDoItems/Filter?UserId=$userId&&ListId=$listId&&SortOrder=asc&&sortBy=isComplete'),
-    );
-
-    if (response.statusCode == 200) {
-      List<dynamic> body = jsonDecode(response.body);
-
-      List<Task> tasks =
-          body.map((dynamic task) => Task.fromJson(task)).toList();
-
-      return tasks;
-    } else {
-      throw "No Lists";
-    }
-  }
-
-  Future<List<Task>> queryTasks(String taskName) async {
-    String? userId = await storage.read(key: 'userId');
-
-    Response response = await get(
-      Uri.parse(
-          'https://elerdigt-api.azurewebsites.net/api/ToDoItems/Filter?UserId=$userId&&taskName=$taskName&&SortOrder=asc'),
-    );
-
-    if (response.statusCode == 200) {
-      List<dynamic> body = jsonDecode(response.body);
-
-      List<Task> tasks =
-          body.map((dynamic task) => Task.fromJson(task)).toList();
-
-      return tasks;
-    } else {
-      throw "No Lists";
-    }
-  }
-
-  Future<void> createTask(CreateTask newTask) async {
-    Map<String, String> headers = {'Content-Type': 'application/json'};
-    String jsEncode;
-    jsEncode = jsonEncode(newTask);
-    await post(
-      Uri.parse('https://elerdigt-api.azurewebsites.net/api/ToDoItems'),
-      headers: headers,
-      body: jsEncode,
-    );
+  void createTask(Task newTask, ListModel list) {
+    _taskBox.add(newTask);
     notifyListeners();
   }
 
-  Future<void> deleteTask(int id) async {
-    Map<String, String> headers = {'Content-Type': 'application/json'};
-    await delete(
-      Uri.parse('https://elerdigt-api.azurewebsites.net/api/ToDoItems/$id'),
-      headers: headers,
-    );
+  getTasks(ListModel list) {
+    List<Task> tasks = <Task>[];
+    tasks = _taskBox.values
+        .where(
+          (x) => x.listKey == list.key,
+        )
+        .toList();
+    return tasks;
+  }
+
+  deleTask(Task task) {
+    task.delete();
     notifyListeners();
   }
 
-  Future<void> editTask(CreateTask editTask, int id) async {
-    Map<String, String> headers = {'Content-Type': 'application/json'};
-    String jsEncode;
-    jsEncode = jsonEncode(editTask);
-    await put(
-      Uri.parse('https://elerdigt-api.azurewebsites.net/api/ToDoItems/$id'),
-      headers: headers,
-      body: jsEncode,
-    );
-    notifyListeners();
-  }
-
-  Future<void> patchTask(bool isFinished, int id) async {
-    Map<String, String> headers = {'Content-Type': 'application/json'};
-    await patch(
-      Uri.parse('https://elerdigt-api.azurewebsites.net/api/ToDoItems/$id'),
-      headers: headers,
-      body: jsonEncode(
-        [
-          {
-            "op": "replace",
-            "path": "/isComplete",
-            "value": isFinished,
-          }
-        ],
-      ),
-    );
-
+  editTask(Task task) {
+    task.save();
     notifyListeners();
   }
 }
