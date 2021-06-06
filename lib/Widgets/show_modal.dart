0968a98b51a636869afe88baed2a2c57aaa36b87
pierreEdimo <pedimonkoe@yahoo.com>
2021-06-06 16:30:47 +0200

    edit show_modal

--- lib/Widgets/show_modal.dart
+++ lib/Widgets/show_modal.dart
@@ -3,22 +3,21 @@ import 'package:flutter/material.dart';
 showCustomModal(
   Widget child,
   BuildContext context,
-  double height,
 ) {
   showModalBottomSheet<void>(
     isDismissible: true,
     isScrollControlled: true,
     context: context,
-    builder: (context) {
-      return Container(
-        color: Color(0xFF737373),
-        child: Container(
-          height: height,
-          padding: EdgeInsets.all(25.0),
-          color: Color(0xFFBFFFFFF),
-          child: child,
-        ),
-      );
-    },
+    builder: (context) => Container(
+      padding: EdgeInsets.only(
+        bottom: MediaQuery.of(context).viewInsets.bottom,
+      ),
+      color: Color(0xFF737373),
+      child: Container(
+        padding: EdgeInsets.all(25.0),
+        color: Color(0xFFBFFFFFF),
+        child: SingleChildScrollView(child: child),
+      ),
+    ),
   );
 }
