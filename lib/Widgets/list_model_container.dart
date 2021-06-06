f1866e83d61041e9024144f01b4ed20a6f67bf41
pierreEdimo <pedimonkoe@yahoo.com>
2021-06-06 16:29:29 +0200

    edit list_model_container

--- lib/Widgets/list_model_container.dart
+++ lib/Widgets/list_model_container.dart
@@ -1,37 +1,86 @@
+import 'package:erledigt/Model/list_detail_arguments.dart';
 import 'package:erledigt/Model/list_model.dart';
+import 'package:erledigt/Screens/list_detail_screen.dart';
+import 'package:erledigt/Service/list_service.dart';
 
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-Widget listModelContainer(ListModel list, BuildContext context) {
-  return Container(
-    child: Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Row(
+class ListModelContainer extends StatefulWidget {
+  final ListModel? list;
+  const ListModelContainer({Key? key, this.list}) : super(key: key);
+
+  @override
+  _ListModelContainerState createState() => _ListModelContainerState();
+}
+
+class _ListModelContainerState extends State<ListModelContainer> {
+  int? taskCount;
+
+  fetchTaskCount(BuildContext context) {
+    setState(() {
+      taskCount = Provider.of<ListService>(context, listen: false)
+          .getTaskCount(widget.list);
+    });
+    return taskCount;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () => Navigator.of(context)
+          .pushNamed(
+            ListDetailScreen.routeName,
+            arguments: ListDetailArguments(widget.list),
+          )
+          .then(
+            (_) => fetchTaskCount(context),
+          ),
+      child: Container(
+        margin: EdgeInsets.only(bottom: 10.0),
+        padding: EdgeInsets.symmetric(
+          horizontal: 10.0,
+          vertical: 25.0,
+        ),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10.0),
+          color: Color(0xffafafafa),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Text(
-              list.emojiUrl!,
-              style: TextStyle(fontSize: 18.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  widget.list!.name!,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18.0,
+                    fontFamily: 'OpenSans',
+                  ),
+                ),
+                fetchTaskCount(context) == 0
+                    ? Text("")
+                    : Text(
+                        "$taskCount",
+                      ),
+              ],
             ),
-            SizedBox(width: 20.0),
+            SizedBox(
+              height: 20.0,
+            ),
             Text(
-              list.name!,
+              widget.list!.description!.isEmpty
+                  ? "No Description added"
+                  : widget.list!.description!,
               style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 16.0,
+                fontSize: 12.0,
               ),
             ),
           ],
         ),
-        list.taskCount! == 0
-            ? Text("")
-            : Text(
-                list.taskCount!.toString(),
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                ),
-              )
-      ],
-    ),
-  );
+      ),
+    );
+  }
 }
