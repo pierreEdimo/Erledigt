c6f8b54d148451ce2e7717b9b048c2eca776b6cd
pierreEdimo <pedimonkoe@yahoo.com>
2021-06-06 16:19:46 +0200

    create a materialcolor black

--- lib/Constants/constants.dart
+++ lib/Constants/constants.dart
@@ -7,6 +7,23 @@ BorderRadius onlyTop = BorderRadius.only(
 
 TextStyle titleStyle = TextStyle(
   fontWeight: FontWeight.bold,
-  fontSize: 18.0,
-  color: Color(0xffbffffff),
+  fontSize: 16.0,
+  fontFamily: 'OpenSans',
 );
+
+const MaterialColor primaryBlack = MaterialColor(
+  _blackPrimaryValue,
+  <int, Color>{
+    50: Color(0xFF000000),
+    100: Color(0xFF000000),
+    200: Color(0xFF000000),
+    300: Color(0xFF000000),
+    400: Color(0xFF000000),
+    500: Color(_blackPrimaryValue),
+    600: Color(0xFF000000),
+    700: Color(0xFF000000),
+    800: Color(0xFF000000),
+    900: Color(0xFF000000),
+  },
+);
+const int _blackPrimaryValue = 0xFF000000;
